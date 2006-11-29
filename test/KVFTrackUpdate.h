fe7e8e19d5f3d2b9448878d947f0e5288bb54e6c
Thomas Speer <sha1-1f3bd195bfa7e608a2a89f916799974d2796a5b8@cern.ch>
2006-11-29 08:46:58 +0000

    Example of single track update with vertex constraint

---
+++ test/KVFTrackUpdate.h
@@ -0,0 +1,31 @@
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+  /**
+   * This is a very simple test analyzer to test the update of a track with
+   * a vertex constraint with the Kalman filter.
+   */
+
+class KVFTrackUpdate : public edm::EDAnalyzer {
+public:
+  explicit KVFTrackUpdate(const edm::ParameterSet&);
+  ~KVFTrackUpdate();
+  
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+
+  virtual void beginJob(edm::EventSetup const&);
+  virtual void endJob();
+
+private:
+
+  std::string trackLabel_; // label of track producer
+};
